afe6b2f262e46441097fa31f917b09b2dad974d9
Grasso Stefano <Stefano.Grasso@gruppomol.it>
2023-12-22 11:04:27 +0100

    add characters controller

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,29 @@
+﻿using DOTNET_RPG.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DOTNET_RPG.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : Controller
+    {
+        private static List<Character> Characters = new List<Character> {
+            new Character(),
+            new Character {Id = 1, Name = "Fobe" }
+            
+        };
+
+        [HttpGet]
+        [Route("GetAll")]
+        public ActionResult<List<Character>> Get()
+        {
+            return Ok(Characters);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Character> GetSingle(int id)
+        {
+            return Ok(Characters.FirstOrDefault(c => c.Id == id));
+        }
+    }
+}
